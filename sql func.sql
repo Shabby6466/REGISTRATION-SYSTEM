32310a7883e221a221372cb8f19d2664cbedd6f5
Muhammad Shoaib Fayyaz <shoaibfayyazf22@nutech.edu.pk>
2024-06-06 04:21:36 +0500

    Add files via upload

---
+++ sql func.sql
@@ -0,0 +1,23 @@
+use OLYMPIAD_REGISTRATION
+
+--updating data
+Update Users SET username = 'Maaz Khan' WHERE users_id = 2;
+
+--ALTER TABLE
+Alter table Eventss
+Add civils int;
+
+--dATA Taransformation
+Select * from Eventss where daate > '2024-09-01' order by daate DESC
+
+
+--Aggregation
+Select created_by, Count(*) AS TotalEvents
+FROM Eventss
+Group by created_by
+
+--joins
+SELECT Users.username, Eventss.event_name, Registration.registration_date
+FROM Registration
+JOIN Users ON Registration.users_id = Users.users_id
+JOIN Eventss ON Registration.Events_ID = Eventss.event_id;
